ccfa756904071c2a43040605ee5980ed9174e9e5
Robert DeVore <deviodigital@gmail.com>
2019-03-04 12:54:40 -0500

    :ok_hand: IMPROVE: Free Delivery Package Rates override

--- admin/dfwc-woocommerce-delivery-fees.php
+++ admin/dfwc-woocommerce-delivery-fees.php
@@ -10,24 +10,59 @@
  */
 
 /**
- * Add delivery fees
- * 
+ * Devide number
+ *
+ * @param float $number
+ * @param float $divide_by
+ * @param float $max
+ * @return float
  * @since 1.0
  */
-function dfwc_add_delivery_fees() {
+function divideNumber( $number, $divide_by, $max ) {
+    if( $number > $max ) {
+        return divideNumber( $number/$divide_by, $divide_by, $max );
+    } else {
+        return $number;
+    }
+}
+
+/**
+ * Delivery Fee Override
+ *
+ * @param [type] $rates
+ * @param [type] $package
+ * @return void
+ * @since 1.0
+ */
+function dfwc_package_rate_override( $rates, $package ) {
 
     global $woocommerce;
 
-    // Set delivery fee.
-	if ( null !== get_option( 'dfwc_settings_delivery_fee_amount' ) && '' !== get_option( 'dfwc_settings_delivery_fee_amount' ) ) {
-        $delivery_cost = get_option( 'dfwc_settings_delivery_fee_amount' );
-    } else {
-        $delivery_cost = '0';
+    // Cart subtotal.
+    $cart_subtotal = WC()->cart->subtotal;
+
+    // Loop through rates.
+    foreach ( $rates as $rate ) {
+        // DFWC Method ID.
+        if ( 'dfwc' === $rate->method_id ) {
+
+            // Get method settings.
+            $delivery_cost = get_option( 'woocommerce_' . $rate->method_id . '_' . $rate->instance_id . '_settings' ); 
+
+            // Get free delivery setting value.
+            $free_delivery = $delivery_cost['free_delivery'];
+
+            // If minimum order amount is met, change to free delivery.
+            if ( '' !== $free_delivery && $free_delivery <= $cart_subtotal ) {
+                $rate->cost = '0';
+            }
+        }
     }
 
-    // Add delivery fee.
-    if ( '0' != $delivery_cost ) {
-        $woocommerce->cart->add_fee( __( 'Delivery', 'dfwc' ), number_format((float)$delivery_cost, 2, '.', ',' ) );
+    return $rates;
+}
+add_filter( 'woocommerce_package_rates', 'dfwc_package_rate_override', 100, 2 );
+
     }
 
 }
