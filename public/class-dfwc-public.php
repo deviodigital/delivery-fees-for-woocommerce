eba673bf07244631e1257d22037cb9f595e49b48
Robert DeVore <deviodigital@gmail.com>
2019-09-29 10:37:23 -0400

    :ok_hand: IMPROVE: Removed public js and css files from loading

--- public/class-dfwc-public.php
+++ public/class-dfwc-public.php
@@ -50,7 +50,7 @@ class DFWC_Public {
 	public function __construct( $plugin_name, $version ) {
 
 		$this->plugin_name = $plugin_name;
-		$this->version = $version;
+		$this->version     = $version;
 
 	}
 
@@ -61,19 +61,7 @@ class DFWC_Public {
 	 */
 	public function enqueue_styles() {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in DFWC_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The DFWC_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/dfwc-public.css', array(), $this->version, 'all' );
+		//wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/dfwc-public.css', array(), $this->version, 'all' );
 
 	}
 
@@ -84,19 +72,7 @@ class DFWC_Public {
 	 */
 	public function enqueue_scripts() {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in DFWC_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The DFWC_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/dfwc-public.js', array( 'jquery' ), $this->version, false );
+		//wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/dfwc-public.js', array( 'jquery' ), $this->version, false );
 
 	}
 
